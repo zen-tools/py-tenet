e5a262fb9448a0659639f3ae4dca0c2bad7b57e2
ZEN <poltavchenko.dmitriy@gmail.com>
2018-10-05 23:32:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,25 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst', 'r') as fp:
+with open('README.md', 'r') as fp:
     long_description = fp.read()
 
 setup(
     name='tenet',
     description='stats.tenet.ua API client',
     long_description=long_description,
+    long_description_content_type="text/markdown",
     version='0.1',
     author='Dmitriy Poltavchenko',
     author_email='poltavchenko.dmitriy@gmail.com',
+    url='https://github.com/zen-tools/py-tenet',
     license='GPL',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['requests']
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GPL License",
+        "Operating System :: OS Independent",
+    ]
 )
